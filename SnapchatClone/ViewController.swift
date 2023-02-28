35a37f4ffa6a3624c1b752623499ee4554065eb8
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-02-28 14:39:06 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Yigit Karakurt on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
